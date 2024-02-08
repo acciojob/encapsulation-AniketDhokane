824e8eced3b232159e3ef092876c47d6c7798846
Aniket <aniketdhokane111@gmail.com>
2024-02-08 17:21:21 +0530

    my changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rwOnly= new RWOnly();
+
+        rwOnly.setA(10);
+        rwOnly.setB(15);
+        System.out.println("The value of a is : "+rwOnly.getA());
+        System.out.println("The value of b id : "+rwOnly.getB());
+    }
+
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private int a=0;
+    private int b=0;
+
+    public int getA(){
+        return a;
+
+    }
+    public int getB(){
+        return b;
+
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
+
