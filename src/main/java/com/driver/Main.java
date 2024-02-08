4fe0f79c34ced028d5efc2f1a9a8c2d645b45c07
Aniket <aniketdhokane111@gmail.com>
2024-02-08 17:59:06 +0530

    my changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         RWOnly rwOnly= new RWOnly();
 
-        //rwOnly.a;
-       // rwOnly.b;
-        
+        rwOnly.a;
+        rwOnly.b;
+
         rwOnly.getA();
         rwOnly.getB();
 
